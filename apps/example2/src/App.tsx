461ed219040c8f4f3fb747d7f9f8471157004014
webstar-code <bhaveshrc2001@gmail.com>
2023-10-09 22:00:37 +0530

    [SSPA-29] add resuable components package

--- apps/example2/src/App.tsx
+++ apps/example2/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { Button } from '@app/components';
+
 
 function App() {
   const [count, setCount] = useState(0);
   return (
     <div className="App">
+      <Button text='asd' />
       <div className="flex justify-center items-center">
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
